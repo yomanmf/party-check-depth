25a5a96b4d2b5931a93a2abf261fa2838f8693a5
Alexander <sanyatest1@yandex.ru>
2016-11-22 01:23:56 +0300

    fixes to make it work

--- cli.js
+++ cli.js
@@ -2,8 +2,14 @@
 
 function cli() {  
     var argv = require('yargs')
-        --url('url', 'site url')
-        --depth('depth', 'depth of how DOM will be checked');    
+        .usage('Usage: $0 [--url=url] [==depth=depth]')
+        .command('url', 'check the ulrs DOM depth')
+        .command('depth', 'the depth of the check')
+        .example('$0 --url=https://... --depth=100', 'check the DOM depth in the url')
+        .epilog('copyright 2016')
+        .help('h')
+        .alias('h', 'help')
+        .argv;
 };
 
  exports.cli = cli;  
