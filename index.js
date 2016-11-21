6560b6c85f2654a7b86e12aedc45f380d25b2eba
Alexander <sanyatest1@yandex.ru>
2016-11-21 23:59:28 +0300

    add function checkDepth

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function checkDepth() {
+    let i = 1,
+    sel = '* > *';
+    while(document.querySelector(sel)) {
+        sel += ' > *';
+        i++;
+    }
+    return i;
+}
+
+a = checkDepth();
+if (a < 100) {
+    console.log('тут должен быть process.exit(0)');
+}
+else { 
+    console.log('тут должен быть process.exit(1)');
+}
