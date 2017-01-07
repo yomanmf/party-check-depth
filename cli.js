f0c19b137da8b71f8c56f60ebb2c01f739e1823f
Alexander <sanyatest1@yandex.ru>
2017-01-07 17:39:25 +0300

    es6 + delete useless code

--- cli.js
+++ cli.js
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
 
-
-const binpath = require('binpath');
-const path = require('path');
-const sell = require('shelljs');
-
 'use strict';
 
-function cli() {  
-    var argv = require('yargs')
-        .usage('Usage: $0 [--url=url] [==depth=depth]')
-        .demand(['url', 'depth'])
-        .command('url', 'check the ulrs DOM depth')
-        .command('depth', 'the depth of the check')
-        .example('$0 --url=https://... --depth=100', 'check the DOM depth in the url')
-        .help('h')
-        .alias('h', 'help')
-        .epilog('copyright 2016')
-        .argv;
-};
+import {url, depth} from './args';
 
- exports.cli = cli;  
+import binPath from 'phantomjs-prebuilt';
+import path from 'path';
+import shell from 'shelljs';
+
+const runPhantom = `${binPath.path} ${url} ${depth}`;
+
+if (shell.exec(runPhantom).code !== 0) {
+    shell.exit(1);
+};
